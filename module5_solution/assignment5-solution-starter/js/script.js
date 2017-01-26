9d556f914b6e8369b37da387c3a4029e3b83e120
bahubalidesai <bahubalidesai0@gmail.com>
2017-01-26 11:15:09 +0530

    updated

--- module5_solution/assignment5-solution-starter/js/script.js
+++ module5_solution/assignment5-solution-starter/js/script.js
@@ -82,9 +82,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-    buildAndShowHomeHTML,// ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+    allCategoriesUrl,
+   buildAndShowHomeHTML
+    , true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -101,7 +101,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-     var chosenCategoryShortName = chooseRandomCategory(categories); // var chosenCategoryShortName = ....
+     var chosenCategoryShortName = chooseRandomCategory(categories);
+     /*console.log(chosenCategoryShortName['short_name']);*/// var chosenCategoryShortName = ....
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
